718b3f531ffc008318c0ea488e85ed3c60f6e590
cavinato <87499876+cavinatto@users.noreply.github.com>
2025-09-03 21:43:32 -0300

    Delete Ateliê Online directory

--- Ateliê Online/static/js/script.js
+++
@@ -1,8 +0,0 @@
-// static/js/script.js
-
-// Exemplo simples de alerta
-function confirmDelete(serviceName) {
-    return confirm(`Tem certeza que deseja excluir o serviço: ${serviceName}?`);
-}
-
-// Em breve você pode adicionar mais scripts aqui, como validações ou requisições AJAX.
