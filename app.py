8bb5c9724926ef1d3378f1c08a86bc17ea35a0d0
Alessandra F Rigonatti <alessandra.rigonatti@aluno.faculdadeimpacta.com.br>
2025-09-15 13:35:54 -0300

    upload de msql para postgre bd -ok web - ok host precisa ser atuakizado com novo front

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ from flask import Flask
 from atelie_online.config import Config
 from atelie_online.models import db
 from atelie_online.controllers.servico_controller import servico_bp
-
+#Biblioteca para pegar time.sleep() - "delay"
+import time
+#OparationlError = tratar erros de exceção
+from sqlalchemy.exc import OperationalError
 
 
 def create_app():
@@ -18,13 +21,32 @@ def create_app():
 
     app.register_blueprint(servico_bp)
 
+    #app_context() -> contexto de aplicação/application context, bloco do app flask ativo, permmite acesso às variáveis e seus resucros
     with app.app_context():
-        db.create_all()
+        
+        #Quantidade de vezes que pode tentar criar ou conectar a um tabela (máximod de tentativas)
+        tries_bd_conc = 5
+        
+        #contador que inicia no valor 1 e segue até o valor da variável tries_bd_conc (5), irá incremetar 1 a cada tentativa
+        for tentar in range(1,tries_bd_conc +1):
+            try:
+                #Lê o SQLALCHEMY_DATABASE_URI
+                db.create_all()
+                break
+            except OperationalError as e:
+                
+                #se o valor de tentativas forem exatemete iguais ao valor de contagem, ensagem de erro
+                if tentar == tries_bd_conc:
+                    raise RuntimeError(f"Falha ao conectar com Banco de dados após {tries_bd_conc} tentativas: {e}")
+                
+                #a cada 1 seegundo, uma nova tentativa de criação
+                time.sleep(1)
 
     return app
 
 if __name__ == '__main__':
+    #comando para criar todas tabelas definidas no model
     app = create_app()
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
 #app.py
--- atelie_online/config.py
+++ atelie_online/config.py
@@ -1,9 +1,14 @@
 import os
+from sqlalchemy import create_engine
 
 # Configurações do banco de dados MySQL
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL","mysql+pymysql://root:Enzo39824360@db:3306/atelie")
+    # Mostra qual database da api o sqlalchemy deve importar, neste caso será bd postgres e psycop2
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL","postgresql+psycopg2://atelie_user:atelie_pass@localhost:5432/atelie")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'chave-secreta'
-
+    
+def get_engine():
+    return create_engine(Config.SQLALCHEMY_DATABASE_URI)
 #atelie_online/config.py
+
